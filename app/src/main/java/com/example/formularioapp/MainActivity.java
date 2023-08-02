df9ec5c5bc6347c21a997d2e7fae132bbb63e496
luispipe <lflondonor@unal.edu.co>
2023-08-01 22:01:30 -0300

    second commit

--- app/src/main/java/com/example/formularioapp/MainActivity.java
+++ app/src/main/java/com/example/formularioapp/MainActivity.java
@@ -3,12 +3,49 @@ package com.example.formularioapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+// AppCompatActivity se encarga de adaptar la vista de la APP
+// a cualquier versión de android posterior a nuestro min SDK
 
+    //Metodo OnCreate se encarga de contruir la vista(una interfaz)
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //set --> actualizar o modificar
+        //get --> obtener
+        EditText name = findViewById(R.id.editTextName);
+        EditText address= findViewById(R.id.editTextTextPostalAddress);
+        EditText phone= findViewById(R.id.editTextPhone);
+        EditText email= findViewById(R.id.editTextTextEmailAddress);
+        RadioGroup radio= findViewById(R.id.radioGroupPlace);
+        CheckBox panel= findViewById(R.id.checkBoxSolar);
+        CheckBox aereo= findViewById(R.id.checkBoxAero);
+        CheckBox otro= findViewById(R.id.checkBoxOther);
+        Button boton= findViewById(R.id.button);
+
+        //Cualquier elemento de la interfaz imagenes, botones, textos
+        //se les puede asociar una acción o evento
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boton.setText(name.getText().toString());
+                name.setText("");
+            }
+        });
+
+
     }
+
+
+
 }
+
