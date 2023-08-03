8fdbb92172aab7e952ff32ae7ca60e2b4f8ac0c6
luispipe <lflondonor@unal.edu.co>
2023-08-03 20:52:13 -0300

    form final commit

--- app/src/main/java/com/example/formularioapp/MainActivity.java
+++ app/src/main/java/com/example/formularioapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.formularioapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+// Se importan todas las librerias que se requieren
+// libreria --> una paquetas de herramientas que necesitamos para que algo funcione
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,13 +19,32 @@ public class MainActivity extends AppCompatActivity {
 // a cualquier versión de android posterior a nuestro min SDK
 
     //Metodo OnCreate se encarga de contruir la vista(una interfaz)
-    @Override
 
+    //Override es igual a sobreescribir
+
+    // Siempre que se defina una clase o función
+    // Se le asigna un nivel de acceso public, protected, private
+
+    //La función onCreate es la que se encarga de construir la interfaz
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Generar una vista vacia o interfaz vacia
         super.onCreate(savedInstanceState);
+        //Cambiamos la interfaz vacia por la que tenemos en nuestro layout
         setContentView(R.layout.activity_main);
         //set --> actualizar o modificar
         //get --> obtener
+
+        /*Siempre que queramos obtener un dato o información
+          de un elemento de interfaz (button, edittext, TextView
+          Table, RadioButton, etc) se debe definir una variable
+          del mismo tipo.
+          Ejemplo: para obtener el texto de un TextView -- TextView nombreX;
+
+          findViewById --> es la funcionalidad que nos permite traer
+          el elemento desde la interfaz por medio de su ID
+        */
+
         EditText name = findViewById(R.id.editTextName);
         EditText address= findViewById(R.id.editTextTextPostalAddress);
         EditText phone= findViewById(R.id.editTextPhone);
@@ -38,12 +59,22 @@ public class MainActivity extends AppCompatActivity {
         Button boton= findViewById(R.id.button);
         TextView resultado= findViewById(R.id.textViewResult);
 
+
         // x=20
         // 1=10
+
         //Cualquier elemento de la interfaz imagenes, botones, textos
         //se les puede asociar una acción o evento
 
+        /* Para generar una acción u evento se llama a el elemento
+        *  y se implementa la funcionalidad setOnClickListener que se encarga
+        *  de escuchar o percibir cuando se clickea sobre el elemento y dispara
+        *  o ejecuta una acción o evento, donde se construye una nueva vista o interfaz
+         * */
         house.setOnClickListener(new View.OnClickListener() {
+
+            //Dentro del onClick es donde se define lo que se quiere
+            //mostrar en la nueva vista que sea diferente a la vista actual
             @Override
             public void onClick(View v) {
                 farm.setChecked(false);
@@ -127,8 +158,22 @@ public class MainActivity extends AppCompatActivity {
                     );
 
 
+                /* Toast es un mensaje emergente que se muestra en l APP para alertar al usuario
+                 para ello se llama a Toast.makeText se le envia el contexto de la APP para
+                 que el identifique en cual interfaz esta para saber donde despliega el mensaje
+                 por esto usamos el getApplicationContext o getBaseContext
+                 (los parametro o valores en java se separan por coma , )
 
-                    Toast.makeText(getApplicationContext(),"Formulario completo",Toast.LENGTH_LONG).show();
+                 *El segundo parametro es el mensaje que queremos dar
+                 (Los textos en java van dentro de comillas dobles "")
+                 *El tercer parametro es el tiempo que se visualizara el mensaje
+                 -Solo hay dos opciones Toast.Length_Long tarda 5 seg
+                 - Toast.Length_short tarda 2 seg
+
+                 Para finalizar se utiliza la funcionalidad show() que lo que hace
+                 es mostrar el mensaje en la interfaz que se detecto
+                  * */
+                    Toast.makeText(getBaseContext(),"Formulario completo",Toast.LENGTH_LONG).show();
                 }
 
             }
